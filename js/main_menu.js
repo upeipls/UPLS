c8c2dc059e3a61b272c191424cb12d7ce7db71d0
Johanna <JojoVes@users.noreply.github.com>
2019-02-09 16:16:32 -0400

    Add main page and start main menu.

--- js/main_menu.js
+++ js/main_menu.js
@@ -3,5 +3,15 @@
  */
 
 function mainMenu() {
-
+	var main_menu = "\
+	<a href=\"main_page.html\">Home</a>\
+	<a href=\"add_student.html\">Add Student</a>\
+	<a href=\"group_email.html\">Group Email</a>\
+	<a href=\"\"></a>\
+	<a href=\"\"></a>\
+	<a href=\"\"></a>\
+	<a href=\"\"></a>\
+	<a href=\"\"></a>\
+	";
+  document.getElementById("main_menu").innerHTML = main_menu;
 }
