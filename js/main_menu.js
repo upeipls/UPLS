5be59ae91eb9f3319c5c6f867dbdd0b9a7c554ae
Johanna <JojoVes@users.noreply.github.com>
2019-02-09 20:55:31 -0400

    Add main menu functionality.
    Fixed css file typo in all html pages.
    Added simple css for basic dropdown functionality.

--- js/main_menu.js
+++ js/main_menu.js
@@ -7,11 +7,18 @@ function mainMenu() {
 	<a href=\"main_page.html\">Home</a>\
 	<a href=\"add_student.html\">Add Student</a>\
 	<a href=\"group_email.html\">Group Email</a>\
-	<a href=\"\"></a>\
-	<a href=\"\"></a>\
-	<a href=\"\"></a>\
-	<a href=\"\"></a>\
-	<a href=\"\"></a>\
+	<a href=\"aggregation.html\">Aggregate</a>\
+	<a href=\"view_student.html\">View Student</a>\
+   <button onmouseover=\"dropdown()\" id=\"dropdown\">Admin Menu</button>\
+   <div id=\"dropdown_items\" class=\"dropdown_items\">\
+     <a href=\"batch_ingest.html\">Batch Ingest</a>\
+     <a href=\"edit_interaction_categories.html\">Interaction Categories</a>\
+     <a href=\"edit_student_categories.html\">Student Categories</a>\
+	</div>\
 	";
   document.getElementById("main_menu").innerHTML = main_menu;
+}
+
+function dropdown() {
+	document.getElementById("dropdown_items").classList.toggle("show");
 }
