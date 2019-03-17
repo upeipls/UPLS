aea72add8a5503042523ff188c429aa07c42f29c
Connor Mayhew <ctmayhew@upei.ca>
2019-03-17 15:59:10 -0300

    search functionality & minor fixes
    adds search and display functionality to the single search page. Fixes external js inclusion order. Some small issues include the searches being case sensitive, further testing required to reveal the cause of some incorrect returns. Search functionality referenced from group email page

--- js/single_search.js
+++ js/single_search.js
@@ -1,9 +1,18 @@
 /*
- * 	This file will handle the JS methods required for the function of the single search page.
+ * 	This file will handle the JS methods required for the function of the single search page. 
+ *  Created 2019/03/04 by Connor Mayhew
+ *  Last Edited 2019/03/15 by Connor Mayhew
  */
  
 let sa = new SheetsApi();
+//!!set key!!-- remove this in final product (at least remove keys in pull requests)	
+var sheetID = "";
+var apiKey = "";
+var clientID = "";
+sa.setKeys(sheetID, apiKey, clientID);
+//!!set key!!
 sa.handleClientLoad();
+
 function updateSignInStatus(isSignedIn) {
     if (isSignedIn) {
     } else {
@@ -12,10 +21,47 @@ function updateSignInStatus(isSignedIn) {
     }
 }
 
- function drawTable() //div id="resultTable"
- {
- }
+//variable for id representing student to be submitted to next page
+var submitID; 
  
+ function loadButtonListeners()
+ //loads event listeners and their functionality onto page buttons
+ {
+	 //these are helpers to call js functions in single_search.html
+	document.getElementById("viewStudentButton").addEventListener("click", function()
+	{
+		submitStudent("view_student.html");
+	});
+	
+	document.getElementById("editStudentButton").addEventListener("click", function() 
+	{
+		submitStudent("edit_student.html");
+	});
+	
+	document.getElementById("viewInteractionButton").addEventListener("click", function()
+	{
+		//select interaction page
+		//submitStudent("");
+	});
+	
+	document.getElementById("editInteractionButton").addEventListener("click", function() 
+	{
+		//edit interaction page
+		//submitStudent("");
+	});
+	
+	document.getElementById("addInteractionButton").addEventListener("click", function() 
+	{
+		//edit interaction page
+		//submitStudent("");
+	});
+	
+	document.getElementById("searchButton").addEventListener("click", function() 
+	{
+		searchStudents();
+	});
+ }
+  
  function showFunctionButton(code)
  /*shows the button for the function requested from main menu
   *invalid code = 00, view student = 10, edit student = 11, view interaction = 20, edit interaction = 21
@@ -47,29 +93,14 @@ function updateSignInStatus(isSignedIn) {
 		 alert("Unrecognized function code " + code + " from main page. Redirecting you back to main page. Please try again.");
 		 window.location.href = "main_page.html";
 	 }
- }
+ } 
  
-  function loadButtonListeners()
- //loads event listeners and their functionality onto page buttons
+ function searchStudents()
+ /*
+  *  Takes user-entered criteria, searches student sheet, and returns data.
+  *  Also 
+  */
  {
-	 //these are helpers to call js functions in single_search.html
-	document.getElementById("viewStudentButton").addEventListener("click", function() {
-
-	});
-	
-	document.getElementById("editStudentButton").addEventListener("click", function() {
-	});
-	
-	document.getElementById("viewInteractionButton").addEventListener("click", function() {
-	});
-	
-	document.getElementById("editInteractionButton").addEventListener("click", function() {
-	});
-	
-	document.getElementById("addInteractionButton").addEventListener("click", function() {
-	});
-	
-	document.getElementById("searchButton").addEventListener("click", function() {
 	//gapi search functionality here
 	//booleans for checking if fields filled, default no
 	var boolID = false;
@@ -84,31 +115,159 @@ function updateSignInStatus(isSignedIn) {
 	boolEmail = Boolean(document.getElementById('emailField').value);
 	
 	//get results of content check
-	if(boolID == true || boolFirst == true || boolLast == true || boolEmail == true)
+	if(boolID || boolFirst || boolLast || boolEmail)
 	{
-		//remove any previous err message
-		document.getElementById("feedBackMessage").classList.toggle("invisible", true);
+		//count entered criteria
+		var numCriteria = [boolEmail, boolFirst, boolID, boolLast].filter(Boolean).length;
 		
-		//submit a search for each field with content 
-	}
-	else
-	{
+		//remove any previous err message
+		clearFeedback();
+		//grab table so that we may display the results
+		var studentTable = document.getElementById("studentTable");
+		
+		//make sure only results from current search will be displayed
+		clearTable(studentTable);
+		
+		//format conditions for search
+		let conditions = [numCriteria];
+		var critIndex = 0;
+		
+		if (boolID)
+		{
+			conditions[critIndex] = {"header":"STUDENT_ID", "value":"" + document.getElementById('idNumberField').value};
+			critIndex++;
+		}
+		
+		if (boolFirst)
+		{		
+			conditions[critIndex] = {"header":"FIRST_NAME", "value":"" + document.getElementById('firstNameField').value};
+			critIndex++;		
+		}
+		
+		if (boolLast)
+		{		
+			conditions[critIndex] = {"header":"LAST_NAME", "value":"" + document.getElementById('lastNameField').value};
+			critIndex++;		
+		}
+		
+		if (boolEmail)
+		{		
+			conditions[critIndex] = {"header":"EMAIL", "value":"" + document.getElementById('emailField').value};
+			critIndex++;		
+		}
+		//start building results table
+		var tableData = "<table border=\"1\"><tr><th>Select</th><th>Student ID</th><th>First Name</th><th>Last Name</th><th>Email</th><th>Year of Study</th></tr>"
+		
+		//submit a search with criteria
+		/*
+		 *
+		 * This GAPI search part needs some review- testing has revealed that the entries are case sensitive
+		 * and some searches give back incorrect results
+		 *
+		 */
+		sa.getSheet("UPLS").then(res =>
+		{
+			let result = sa.selectFromTableWhereConditions(res, ["STUDENT_ID","FIRST_NAME","LAST_NAME","EMAIL", "YOS"], conditions, 1).slice(1);
+			
+			//if no results, tell user
+			if(result.length < 1)
+			{
+				feedback("There are no matches for this entry.");
+				//end execution of function 
+				return;
+			}
+			
+			var row;
+			var selectButton;
+			var selectCell;
+			var idCell;
+			var firstNameCell;
+			var lastNameCell;
+			var emailCell;
+			var yearCell;
+			
+			for (let i = 0; i < result.length; i++) 
+			{
+				//format received data into table here
+				//table format is:
+				// | Select (Btn) | STUDENT_ID | FIRST_NAME | LAST_NAME | EMAIL | YOS |
+				//create new row for every student
+				var row = studentTable.insertRow(-1);
+						
+				//create cells
+				selectCell = row.insertCell(0);
+				idCell = row.insertCell(1);
+				firstNameCell = row.insertCell(2);
+				lastNameCell = row.insertCell(3);
+				emailCell = row.insertCell(4);
+				yearCell = row.insertCell(5);
+				
+				//create button for selection of student, make it select associated student id on click
+				selectButton = document.createElement("BUTTON");
+				selectButton.addEventListener("click", function()
+				{
+					selectStudent(result[i][0]);
+				});
+				
+				//add text
+				selectButton.appendChild(document.createTextNode("XXXX"));				
+				
+				//add values to cells
+				selectCell.appendChild(selectButton);
+				idCell.innerHTML = result[i][0];
+				firstNameCell.innerHTML = result[i][1];
+				lastNameCell.innerHTML = result[i][2];
+				emailCell.innerHTML = result[i][3];
+				yearCell.innerHTML = result[i][4];			
+			}
+		});
+	} else {
 		//if nothing entered, tell user
-		document.getElementById("feedBackMessage").innerHTML = "At least one field must be given a value.";
-		document.getElementById("feedBackMessage").classList.toggle("invisible", false);
+		feedback("At least one field must be given a value.");
 	}
-	
-	//getting values
-	var idNumber = document.getElementById('idNumberField').value;
-	var firstName = document.getElementById('firstNameField').value;
-	var lastName = document.getElementById('lastNameField').value;
-	var email = document.getElementById('emailField').value;
-
-	});
  }
  
- function submitStudent(id, address)
+ function clearFeedback()
  {
-	 localStorage.setItem("studentID", id);
-	 window.location.href = address;
+	 document.getElementById("feedBackMessage").innerHTML = "";
+	 document.getElementById("feedBackMessage").classList.toggle("invisible", true);
+ }
+ 
+ function feedback(message)
+ {
+	 document.getElementById("feedBackMessage").innerHTML = message;
+	 document.getElementById("feedBackMessage").classList.toggle("invisible", false);
+ }
+ 
+ function clearTable(table)
+ {
+	 for (let i = table.rows.length-1; i > 0; i--)
+	 {
+		 table.deleteRow(i);
+	 }
+ }
+ 
+ function selectStudent(id)
+ {
+	 feedback("Selected student with ID: " + id);
+	 submitID = id;
+ }
+ 
+ function getSelectedStudent()
+ {
+	 return submitID;
+ }
+ 
+ function submitStudent(address)
+ {
+	 //ensure a student has been selected before submission
+	 if (submitID in window)
+		{
+			feedback("You must select a student in order to continue.");
+		}
+		else
+		{
+			 localStorage.setItem("studentID", getSelectedStudent());
+			 window.location.href = address;
+		}
  }
